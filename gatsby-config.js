644c2ce6e72d37967a7d16ca7f88dc9cc08c161a
Thierry Touin <thierrytouin@gmail.com>
2022-01-06 22:35:07 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteMetadata: {
+    title: "Thierry Touin",
+    description: "Site personel",
+    author: "TTO",
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/contents/`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/images`,
+        name: 'images',
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+  ],
+}
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const About = () => {
+  return (
+    <Layout>
+    <div>
+      <h1>About Page</h1>
+      <p>Ibas is a web developer, teacher and a creative writer.</p>
+    </div>
+    </Layout>
+  )
+}
+
+export default About
