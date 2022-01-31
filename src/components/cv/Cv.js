09b348d9fa1c4c2ccc0d227d0d486ca726d24ba2
Thierry Touin <thierrytouin@gmail.com>
2022-01-31 22:58:26 +0100

    test cv

--- src/components/cv/Cv.js
+++ src/components/cv/Cv.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-//import {CssBaseline, withStyles} from '@material-ui/core';
 import MyInfo from "./MyInfo";
 import Employments from "./Employments";
 //import NameCard from "./NameCard";
