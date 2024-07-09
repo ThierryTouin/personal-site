86c8e23a3da6269e5a5a54a4524f8598454a8ebf
Thierry <thierrytouin@gmail.com>
2024-07-09 22:48:11 +0200

    test

--- src/components/mqtt/mqtt-page1.js
+++ src/components/mqtt/mqtt-page1.js
@@ -11,7 +11,7 @@ const MqttComponent = () => {
       console.log('Connecté au broker MQTT');
 
       // Publier un message
-      client.publish('tto/page1', ''+new Date().getTime(), { retain: true });
+      client.publish('tto/page1', new Date().getTime(), { retain: true });
     });
 
     client.on('error', (error) => {
